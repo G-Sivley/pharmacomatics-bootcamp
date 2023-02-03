b5494c4869a14b06e5e6d1cb51af284f54213e9f
Grant Sivley <grant.sivley@gmail.com>
2023-02-03 15:03:25 -0600

    add custom colors in tailwind

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,21 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className="text-3xl underline">Hello!</h1>
-      </header>
+      <div className='bg-background-blue'>
+      <h1 className='text-white p-10'>WOW</h1>
+
+      </div>
     </div>
   );
 }
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        "background-blue": "#2B4865",
+        "deep-blue": "#002B5B",
+        "light-blue": "#256D85",
+        accent: "#8FE3CF",
+      },
+    },
   },
   plugins: [],
-}
+};
