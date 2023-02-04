a6926750804586bfaa4127ca83bdabe7ce3454d2
Grant Sivley <grant.sivley@gmail.com>
2023-02-04 14:03:41 -0600

    createbrowserrouter

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Home from "./routes/Home";
+import Courses from "./routes/Courses.js";
+import Articles from "./routes/Articles";
+import Login from "./routes/Login";
+import SignUp from "./routes/SignUp";
+import ErrorPage from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "courses/",
+    element: <Courses />,
+  },
+  {
+    path: "articles/",
+    element: <Articles />,
+  },
+  {
+    path: "login/",
+    element: <Login />,
+  },
+  {
+    path: "signup/",
+    element: <SignUp />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>
 );
