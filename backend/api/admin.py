da63839435a6f7835dffe9425dc91afa1d25a7e3
Grant Sivley <grant.sivley@gmail.com>
2023-02-04 09:34:43 -0600

    created article model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User
+from .models import Article, User
 
+
+admin.site.register(Article)
 admin.site.register(User)
--- backend/api/models.py
+++ backend/api/models.py
@@ -13,3 +13,19 @@ class User(models.Model):
     # This returns the user email instead of <User0001>
     def __str__(self):
         return self.email
+    
+
+# Article model
+class Article(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    subtitle = models.CharField(max_length=100)
+    date_created = models.DateField(auto_now_add=True)
+
+    # Need to link with user
+    author = models.CharField(max_length=50)
+
+    content = models.TextField()
+    likes = models.PositiveSmallIntegerField(default=1)
+
+    def __str__(self):
+        return self.title
