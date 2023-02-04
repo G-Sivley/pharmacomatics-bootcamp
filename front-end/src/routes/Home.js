24ea7629a81b71b3618ba6d9cd345ea591cdfa95
Grant Sivley <grant.sivley@gmail.com>
2023-02-04 14:48:48 -0600

    started making landing page

--- front-end/src/routes/Home.js
+++ front-end/src/routes/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="bg-background-blue text-white">
+        <div className="py-72">
+          <h1 className="text-7xl font-bold mb-5">Pharmacomatics Bootcamp</h1>
+          <h2 className="text-3xl font-light text-accent">
+            Your place to gain practical pharmacy informatics experience
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
