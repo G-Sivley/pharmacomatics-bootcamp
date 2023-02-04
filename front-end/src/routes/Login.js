1bc562b78345cb8753932fe065d6f90055ff4956
Grant Sivley <grant.sivley@gmail.com>
2023-02-04 15:45:33 -0600

    create login

--- front-end/src/routes/Login.js
+++ front-end/src/routes/Login.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import ForwardIcon from '@mui/icons-material/Forward';
+import {Link} from "react-router-dom"
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="py-20 border my-20 mx-72 border-gray-200 rounded-lg shadow">
+      <h1 className="pb-8 text-3xl font-bold">
+        Welcome
+      </h1>
+      <ul className="">
+        <li className="">
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+        </li>
+        <li className="py-3">
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+        </li>
+        <li className="py-1">
+          <Button
+            endIcon={<ForwardIcon />}
+            size="large" 
+            variant="contained"
+            onClick={() => {
+              alert("clicked");
+            }}
+          >
+            Login
+          </Button>
+        </li>
+      </ul>
 
-export default Login
+      <p className="pt-20">Need an account? <Link className="text-accent font-bold underline" to="/signup/">Sign up</Link> here!</p>
+    </div>
+
+  );
+};
+
+export default Login;
